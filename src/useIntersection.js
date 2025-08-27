946ef92f3db10b5d480f65243901b2da34acf44c
marina <marina@starbit.co.uk>
2025-08-27 18:08:16 +0100

    add active menu item styling and logic

---
+++ src/useIntersection.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+
+export default function useIsVisible(ref) {
+  const [isIntersecting, setIntersecting] = useState(false);
+  useEffect(() => {
+    // Create an IntersectionObserver to observe the ref's visibility
+    const observer = new IntersectionObserver(([entry]) => {
+      setIntersecting(entry.isIntersecting)
+    },
+    {rootMargin: '-100px'},
+    );
+    // Start observing the element
+    observer.observe(ref.current);
+    // Cleanup the observer when the component unmounts or ref changes
+    return () => {
+      observer.disconnect();
+    };
+  }, [ref]);
+  return isIntersecting;
+}
